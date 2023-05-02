f183cdd97aabe8e96ba5a0d491a7754e789400e6
dougdrumog36 <103113528+dougdrumog36@users.noreply.github.com>
2023-05-02 11:01:01 -0300

    Update series.js

--- src/data/series.js
+++ src/data/series.js
@@ -30,6 +30,37 @@ const DATA =
         nota: '7,0',
         imagem: 'last.jpg'
     },
+
+    {
+        id: '06',
+        nome: 'The Boys',
+        nota: '9,2',
+        imagem: 'boys.jpg'
+    },
+    {
+        id: '07',
+        nome: 'Lucifer',
+        nota: '8,7',
+        imagem: 'lucifer.jpg'
+    },
+    {
+        id: '08',
+        nome: 'The 100',
+        nota: '6,2',
+        imagem: 'the100.jpg'
+    },
+    {
+        id: '09',
+        nome: 'The Umbrella Academy',
+        nota: '7,2',
+        imagem: 'umbrella.jpg'
+    },
+    {
+        id: '10',
+        nome: 'The Walking Dead',
+        nota: '9,7',
+        imagem: 'walkingdead.jpg'
+    },
     
 ]
 
